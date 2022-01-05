2b5dd72519ebaea398d2606576082021eb4ef1f5
Vishnubaba <anna_vladimir@mail.ru>
2022-01-05 21:03:53 +0500

    Добавлена стилизация виджетов формы авторизации

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 //import 'package:google_fonts/google_fonts.dart';
-
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    const borderStyle = OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(36)),
+          borderSide: BorderSide(
+          color: const Color(0xffbbbbbb), width: 2));
+    const linkTextStyle = TextStyle (
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+    color: Color(0xFF0079D0));
+
     return MaterialApp(
       home: Scaffold(
           body: Container (
@@ -20,13 +27,16 @@ class MyApp extends StatelessWidget {
               SizedBox(height: 60,),
               SizedBox(width: 110, height: 84, child: Placeholder(),),
               SizedBox(height: 20,),
-              Text('Введите логин в виде 10 цифр номера телефона'),
+              Text('Введите логин в виде 10 цифр номера телефона',
+              style: TextStyle (fontSize: 16, color: Color.fromRGBO(0, 0, 0, 0.6)),),
               SizedBox(height: 20,),
               const SizedBox(width: 224,
                 child: TextField(
                   decoration: InputDecoration (
                     filled: true,
-                    fillColor: Color(0xFFeceff1),
+                    fillColor: Color(0xffeceff1),
+                    enabledBorder: borderStyle,
+                    focusedBorder: borderStyle,
                     labelText: 'телефон',
                   ),
                 ),
@@ -38,16 +48,30 @@ class MyApp extends StatelessWidget {
                 decoration: InputDecoration (
                 filled: true,
                 fillColor: Color(0xFFeceff1),
+                  enabledBorder: borderStyle,
+                  focusedBorder: borderStyle,
                 labelText: 'пароль',
                 ),
                 ),
                 ),
               SizedBox(height: 28,),
-            SizedBox(width: 154, height: 42, child: ElevatedButton(onPressed: () {}, child: Text('Войти'))),
-              SizedBox(height: 62,),
-              InkWell(child: Text('Регистрация'),onTap: () {} ),
+            SizedBox(width: 154, height: 42, child:
+              ElevatedButton(onPressed: () {},
+                child: Text('Войти'),
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xFF0079D0),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(36),
+                  ),
+                ) ,
+            )
+            ),
+              SizedBox(height: 32,),
+              InkWell(child: const Text('Регистрация', style: linkTextStyle, ),
+              onTap: () {} ),
               SizedBox(height: 20,),
-              InkWell(child: Text('Забыли пароль'),onTap: () {} ), 
+              InkWell(child: Text('Забыли пароль?', style: linkTextStyle,),
+              onTap: () {} ),
 
 
     ], ),
